e0f6ed0add6de0fd2e692b0789bf7d5c9e7f1132
matthew <homekeeper89@gmail.com>
2020-01-16 01:25:22 +0900

    [new] 기본 세팅

---
+++ pages/erik.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{message}}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    data(){
+      return{
+        message:'hello world'
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
