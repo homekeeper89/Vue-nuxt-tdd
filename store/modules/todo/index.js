8ff1620d7e5bf0de738fa6c3890053c6f0dc8313
matthew <homekeeper89@gmail.com>
2020-01-19 17:40:21 +0900

    [fix] state 테스트

--- store/modules/todo/index.js
+++ store/modules/todo/index.js
@@ -8,7 +8,10 @@ const defaultState = {
 
 const inBrowser = typeof window !== 'undefined';
 // if in browser, use pre-fetched state injected by SSR
-const state = (inBrowser && window.__INITIAL_STATE__) ? window.__INITIAL_STATE__.page : defaultState;
+// const state = (inBrowser && window.__INITIAL_STATE__) ? window.__INITIAL_STATE__.page : defaultState;
+const state = {
+  hello : 'hello world'
+}
 
 export default {
   state,
