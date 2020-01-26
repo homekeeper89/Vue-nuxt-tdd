2f0c1217e91832b669e872fa627ddfcae037923c
matthew <homekeeper89@gmail.com>
2020-01-26 17:01:29 +0900

    [new] dialog 추가

---
+++ components/todo/dialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="">
+    <UiDialog>
+      <template #title="slotProps">
+        제목이에요
+      </template>
+      <template #content="slotProps">
+        <UiInput :someValue="someValue" :method="getValue"></UiInput>
+      </template>
+    </UiDialog>
+  </div>
+</template>
+
+<script>
+import UiDialog from '@/components/ui/ui-dialog'
+import UiInput from '@/components/ui/ui-input'
+export default {
+  components:{
+    UiDialog, UiInput
+  },
+  data(){
+    return{
+      someValue:'hello World'
+    }
+  },
+  methods:{
+    getValue(childValue){
+      console.log(childValue)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/ui/ui-dialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on }">
+        <v-btn color="red lighten-2" dark v-on="on">Click Me</v-btn>
+      </template>
+      <v-card>
+        <div class="dialog-title">
+          <v-card-title class="headline grey lighten-2" primary-title>
+            <slot name="title">
+              기본 제목
+            </slot>
+          </v-card-title>
+        </div>
+        <div class="dialog-content">
+          <v-card-text>
+            <slot name="content">
+              기본 이름
+            </slot>
+          </v-card-text>
+        </div>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">I accept</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
