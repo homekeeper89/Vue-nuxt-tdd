b3d540b00af4d9fecb18c1b096b64fa549f2687f
matthew <matthew@cupist.com>
2020-02-04 20:11:57 +0900

    [fix] rowClick 기능 추가

--- components/todo/Body.vue
+++ components/todo/Body.vue
@@ -33,8 +33,8 @@ export default {
     return {};
   },
   methods: {
-    onRowClick(itemId) {
-      this.method(itemId);
+    onRowClick(item) {
+      this.method(item);
     }
   },
   mounted() {
