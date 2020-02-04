1208112ee5cd9b6bafead5a0fea73a882d1ce6b1
matthew <matthew@cupist.com>
2020-02-04 20:43:53 +0900

    [fix] addNewTodo 추가
    rowClick 이벤트 만드는 중

--- cypress/integration/test/todo-main.spec.js
+++ cypress/integration/test/todo-main.spec.js
@@ -8,17 +8,23 @@ describe("todo main test", () => {
     cy.get("[data-cy=todo-table]").find('tr').its('length').should('eq', 3)
   });
 
-  it('Click row and add something', ()=>{
+  it('Click Me and add something', ()=>{
     cy.get("[data-cy=dialog-clickMe]").click()
     cy.get("[data-cy=todo--input--content").type("some words")
     cy.get("[data-cy=todo--input--title").type("some title")
+    cy.get("[data-cy=todo--input--title").find('input').focus().blur()
     cy.get("[data-cy=todo--btn__agree]").click()
+    cy.wait(2000)
+    cy.get("[data-cy=todo-table]").last().contains('td', 'some words')
   })
 
-  it('responds with the stub', () => {
-    cy.server();
-    cy.route('https://jsonplaceholder.typicode.com/todos/1', { id: 1, name: 'Phoebe'});
-    cy.visit('/todo');         // the url to visit in your app
+  it('Row Click and show data', ()=>{
+    cy.get("[data-cy=todo-table]").first().click()
     
-  });
+  })
+  // it('responds with the stub', () => {
+  //   cy.server();
+  //   cy.route('https://jsonplaceholder.typicode.com/todos/1', { id: 1, name: 'Phoebe'});
+  //   cy.visit('/todo');         // the url to visit in your app
+  // });
 });
