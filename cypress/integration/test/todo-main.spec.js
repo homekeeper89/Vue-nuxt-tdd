1c2c66223ca0aa77c7bd29a3ee48c79971c1766e
matthew <matthew@cupist.com>
2020-02-09 12:54:01 +0900

    [fix] text 가져오는것 드디어 해냈다!!

--- cypress/integration/test/todo-main.spec.js
+++ cypress/integration/test/todo-main.spec.js
@@ -25,7 +25,7 @@ describe("todo main test", () => {
     cy.get("[data-cy=todo__dialog__input--title]").find('input').invoke('text').then((text)=>{
       cy.log(">>>>>>", text)
     })
-    cy.get("[data-cy=todo__dialog__input--title]").find('input').invoke('text').should("contain", title);
+    cy.get("[data-cy=todo__dialog__input--title]").find('input').invoke('val').should("contain", title);
     // cy.get("[data-cy=todo__dialog__input--content]").should('contain', contents)
 
   })
