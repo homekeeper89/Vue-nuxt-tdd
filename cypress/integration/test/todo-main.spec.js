2cc07c90e727f51d046f33d65bd3bdc39cc41d66
matthew <matthew@cupist.com>
2020-02-09 16:09:36 +0900

    [fix] 오탈자 수정 > 대괄호가..

--- cypress/integration/test/todo-main.spec.js
+++ cypress/integration/test/todo-main.spec.js
@@ -10,9 +10,9 @@ describe("todo main test", () => {
 
   it('Click Me and add something', ()=>{
     cy.get("[data-cy=todo__dialog__btn--register]").click()
-    cy.get("[data-cy=todo__dialog__input--content").type("some words")
-    cy.get("[data-cy=todo__dialog__input--title").type("some title")
-    cy.get("[data-cy=todo__dialog__input--title").find('input').focus().blur()
+    cy.get("[data-cy=todo__dialog__input--content]").type("some words")
+    cy.get("[data-cy=todo__dialog__input--title]").type("some title")
+    cy.get("[data-cy=todo__dialog__input--title]").find('input').focus().blur()
     cy.get("[data-cy=todo__dialog__btn--accept]").click()
     cy.wait(2000)
     cy.get("[data-cy=todo__table]").last().contains('td', 'some words')
