c295a09fde1d1752cb02f10d2c692e86cf613556
matthew <matthew@cupist.com>
2020-02-18 19:40:01 +0900

    [add] iframe용 vue

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <h1>XHR in iframe</h1>
+    <iframe src="https://jsonplaceholder.cypress.io/" data-cy="the-frame"></iframe>
+  </div>
+</template>
+<style scoped>
+iframe {
+  width: 300%;
+  height: 300%;
+}
+.main{
+  position: absolute;
+  height: 100%;
+}
+</style>
