7133ae882bad4a5330274e15ce47a5e8aa3e7ff0
matthew <matthew@cupist.com>
2020-03-13 00:44:30 +0900

    [add] 개발 환경 포트 변경
    flask 기본 서버 포트와 충돌나기때문

--- cypress/integration/test/user.spec.js
+++ cypress/integration/test/user.spec.js
@@ -4,8 +4,8 @@ describe('유저에 관한 모든 테스트', ()=>{
   })
   it('등록확인', () => {
     cy.get("[data-cy=user__register__form--btn]").click()
-    cy.get("[data-cy=user_register__id]").type("wkdgndldi")
-    cy.get("[data-cy=user_register__pwd]").type("password")
-    cy.get("[data-cy=user_register__pwd--re]").type("password")
+    cy.get("[data-cy=user__register__id]").type("wkdgndldi")
+    cy.get("[data-cy=user__register__pwd]").type("password")
+    cy.get("[data-cy=user__register__pwd--re]").type("password")
   })
 })
