c27e7025f4e19dcd9b47330f79c44712c7f8985a
matthew <matthew@cupist.com>
2020-03-14 15:04:10 +0900

    [modify] 비즈니스 로직 수정

--- cypress/integration/test/user.spec.js
+++ cypress/integration/test/user.spec.js
@@ -3,8 +3,8 @@ describe('유저에 관한 모든 테스트', () => {
     cy.visit('/user')
   })
   it('등록확인', () => {
-    cy.get("[data-cy=user__register__form--btn]").click()
     cy.get("[data-cy=user__register__id]").type("wkdgndldi")
     cy.get("[data-cy=user__register__pwd]").type("password")
+    cy.get("[data-cy=user__login__btn]").click()
   })
 })
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -2,17 +2,17 @@
   <div class="user--index">
     <div>
       <span>ID 입력</span>
-      <UiInput data-cy="user__register__id" :method="getValue"></UiInput>
+      <UiInput data-cy="user__register__id" :method="getIdValue"></UiInput>
     </div>
     <div>
       <span>패스워드 입력</span>
-      <UiInput data-cy="user__register__pwd" :method="getValue"></UiInput>
+      <UiInput data-cy="user__register__pwd" :method="getPwdValue"></UiInput>
     </div>
     <div>
       <span>로그인 등록 버튼</span>
     </div>
     <div>
-      <UiButton :property="btnRegisterObj" :method="register" data-cy="user__register__form--btn">
+      <UiButton :property="btnRegisterObj" :method="loginUserBtn" data-cy="user__login__btn">
         <template #button_name>등록</template>
       </UiButton>
     </div>
@@ -21,6 +21,7 @@
 <script>
 import UiButton from "@/components/ui/ui-button-primary";
 import UiInput from "@/components/ui/ui-input";
+import {mapActions} from 'vuex';
 
 export default {
   components: {
@@ -35,10 +36,13 @@ export default {
     };
   },
   methods: {
-    register() {
-      alert("hhh");
+    loginUserBtn() {
+      this.loginUser()
     },
-    getValue(val){
+    getIdValue(val){
+      console.log(val)
+    },
+    getPwdValue(val){
       console.log(val)
     }
   }
