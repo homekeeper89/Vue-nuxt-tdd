348a4e1e4206233b2b0e7646fb8f5f91cce7a227
matthew <matthew@cupist.com>
2020-03-23 23:18:15 +0900

    [add] data-cy 추가

--- cypress/integration/test/user.spec.js
+++ cypress/integration/test/user.spec.js
@@ -6,5 +6,7 @@ describe('유저에 관한 모든 테스트', () => {
     cy.get("[data-cy=user__register__id]").type("wkdgndldi")
     cy.get("[data-cy=user__register__pwd]").type("password")
     cy.get("[data-cy=user__login__btn]").click()
+
+    cy.get("[data-cy=user__register__dialog--form]").click()
   })
 })
