1f741716e660600b69f77bd70a436d543148ee16
matthew <matthew@cupist.com>
2020-03-24 22:16:44 +0900

    [modify] title 체크 테스트 수정

--- cypress/integration/test/user.spec.js
+++ cypress/integration/test/user.spec.js
@@ -8,6 +8,9 @@ describe('유저에 관한 모든 테스트', () => {
     cy.get("[data-cy=user__login__btn]").click()
 
     cy.get("[data-cy=user__register__dialog--form]").click()
-    cy.get("[data-cy=dialog__form--title]").text()
+
+    cy.get("[data-cy=dialog__form--title]").then($val=>{
+      assert.equal($val.text, 'User Profile')
+    })
   })
 })
