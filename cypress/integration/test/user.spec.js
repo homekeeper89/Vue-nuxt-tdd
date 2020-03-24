5350a47f1bf25761d4164ff2cc3556a4507a67ab
matthew <matthew@cupist.com>
2020-03-24 22:10:01 +0900

    [add] dialog-title 테스팅 추가

--- cypress/integration/test/user.spec.js
+++ cypress/integration/test/user.spec.js
@@ -8,5 +8,6 @@ describe('유저에 관한 모든 테스트', () => {
     cy.get("[data-cy=user__login__btn]").click()
 
     cy.get("[data-cy=user__register__dialog--form]").click()
+    cy.get("[data-cy=dialog__form--title]").text()
   })
 })
